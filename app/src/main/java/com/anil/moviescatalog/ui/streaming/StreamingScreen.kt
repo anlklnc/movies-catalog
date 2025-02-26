41cd0c83c524d3d0ff661bec7cc48bfba8ce389b
Anil Kilinc <anilkilinc@scorpapp.com>
2025-02-26 14:56:28 +0300

    screen timeout prevented on StreamingScreen

--- app/src/main/java/com/anil/moviescatalog/ui/streaming/StreamingScreen.kt
+++ app/src/main/java/com/anil/moviescatalog/ui/streaming/StreamingScreen.kt
@@ -1,6 +1,12 @@
 package com.anil.moviescatalog.ui.streaming
 
+import android.app.Activity
+import android.content.Context
 import android.content.res.Configuration
+import android.os.Build
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
@@ -29,8 +36,27 @@ fun StreamingScreen (
 ) {
     val configuration = LocalConfiguration.current
     val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+    val context = LocalContext.current
 
-    vm.initializePlayer(LocalContext.current)
+    DisposableEffect(Unit) {
+        val window = (context as? Activity)?.window
+        // screen always on
+        window?.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        // hide system UI in landscape mode
+        if (isLandscape) {
+            displaySystemUi(context, false)
+        } else {
+            displaySystemUi(context, true)
+        }
+
+        onDispose {
+            window?.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+            displaySystemUi(context, true)
+        }
+    }
+
+    vm.initializePlayer(context)
 
     if (isLandscape) {
         Box(modifier = Modifier.fillMaxSize()) {
@@ -76,6 +102,30 @@ fun ExoPlayerView(exoPlayer: ExoPlayer?, isLandscape: Boolean = false) {
             .conditional(isLandscape){fillMaxSize()}
             .background(Color.Black)
     )
+}
+
+@Suppress("DEPRECATION")
+fun displaySystemUi(context: Context, show: Boolean) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        val controller = (context as? Activity)?.window?.insetsController
+        if (show) {
+            controller?.show(WindowInsets.Type.systemBars())
+        } else {
+            controller?.hide(WindowInsets.Type.systemBars())
+        }
+    } else {
+        val visibility = if (show) {
+            View.SYSTEM_UI_FLAG_VISIBLE
+        } else {
+            View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        }
+        (context as? Activity)?.window?.decorView?.systemUiVisibility = visibility
+    }
 }
 
 fun Modifier.conditional(condition : Boolean, modifier : Modifier.() -> Modifier) : Modifier {
